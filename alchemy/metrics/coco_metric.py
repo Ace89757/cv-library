1bae5d0cb205f46d1efe7d9f3b900ee6b5df9648
Ace <aixinjin@outlook.com>
2024-03-04 21:10:12 +0800

    feat: Added recall@.5/.75 metric, average metrics are calculated according to class weights.

--- alchemy/metrics/coco_metric.py
+++ alchemy/metrics/coco_metric.py
@@ -23,6 +23,19 @@ from ..registry import METRICS
 from ..utils import heading_line
 
 
+def softmax(x):
+    max_num = np.max(x)
+
+    x = x - max_num
+
+    if len(x.shape) > 1:
+        x=np.exp(x)/ np.sum(np.exp(x), axis=1).reshape(-1, 1)
+    else:
+        x=np.exp(x) / np.sum(np.exp(x))
+        
+    return x
+
+
 @METRICS.register_module()
 class AlchemyCocoDet2dMetric(CocoMetric):
     def __init__(self,
@@ -45,10 +58,12 @@ class AlchemyCocoDet2dMetric(CocoMetric):
         
         # 获取每个类别的图片和bbox的数量
         self.cat_infos = None
+        self.cat_weights = None
         
         self.work_dir = None
         self.current_epoch = -1  # 当前的epoch
-        self.best = dict()
+        self.best_coco = dict()
+        self.best_alchemy = dict()
 
         # 101 steps, from 0% to 100% recall.
         self.rec_interp = np.linspace(0, 1, 101)
@@ -58,14 +73,20 @@ class AlchemyCocoDet2dMetric(CocoMetric):
     def _obtain_cat_infos(self) -> dict:
         cat_infos = dict()
 
-        for cat in self.dataset_meta['classes']:
+        for cat in self.dataset_meta['classes']:  # dataset_meta就是metainfo, 其classes的顺序要与annotation中一致
             cat_id = self._coco_api.get_cat_ids(cat_names=[cat])
             img_ids = self._coco_api.get_img_ids(cat_ids=cat_id)
             ann_ids = self._coco_api.get_ann_ids(img_ids=img_ids, cat_ids=cat_id, iscrowd=None)
 
             cat_infos[cat] = dict(anns=len(ann_ids), imgs=len(img_ids))
 
-        return cat_infos
+        class_counts = np.array([cat_infos[cat]['anns'] for cat in self.dataset_meta['classes']])  
+        class_weights = softmax(np.log(class_counts + 20000))  # +20000是防止类别数量太少，导致权重为0
+
+        for idx, cat in enumerate(cat_infos):
+            cat_infos[cat]['weight'] = class_weights[idx]
+
+        return cat_infos, class_weights.reshape(-1)
 
     def process(self, data_samples: Sequence[dict]) -> None:
         """Process one batch of data samples and predictions. The processed
@@ -133,13 +154,13 @@ class AlchemyCocoDet2dMetric(CocoMetric):
 
         # handle lazy init
         if self.cat_ids is None:
-            self.cat_ids = self._coco_api.get_cat_ids(cat_names=self.dataset_meta['classes'])
+            self.cat_ids = self._coco_api.get_cat_ids(cat_names=self.dataset_meta['classes'])  # dataset_meta['classes']的顺序要与annotations['categories']的顺序一致
 
         if self.img_ids is None:
             self.img_ids = self._coco_api.get_img_ids()
 
         if self.cat_infos is None:
-            self.cat_infos = self._obtain_cat_infos()
+            self.cat_infos, self.cat_weights = self._obtain_cat_infos()
 
         # convert predictions to coco format and dump to json file
         result_files = self.results2json(preds, outfile_prefix)
@@ -204,6 +225,11 @@ class AlchemyCocoDet2dMetric(CocoMetric):
         heading_line(log='COCO Metric', logger=logger)
         coco_eval.summarize()
 
+        classwise_metrics = []
+        headers = [
+                'category', 'images', 'bboxes', 'weight', 'AP', 'AR', 'F1', 'AP@.5', 'R@.5', 'AP@.75', 'R@.75', 'AP@small', 'AP@medium', 'AP@large'
+            ]
+
         # 获取每个类别的KPI
         if self.classwise:
             # Compute per-category AP from https://github.com/facebookresearch/detectron2/
@@ -219,12 +245,14 @@ class AlchemyCocoDet2dMetric(CocoMetric):
             results_per_category = []
 
             for idx, cat_id in enumerate(self.cat_ids):
+                cls_metric = []
+
                 # area range index 0: all area ranges
                 # max dets index -1: typically 100 per image
                 nm = self._coco_api.loadCats(cat_id)[0]
 
                 cat_name = nm["name"]
-                t = [f'{cat_name}', f'{self.cat_infos[cat_name]["imgs"]}', f'{self.cat_infos[cat_name]["anns"]}']
+                t = [f'{cat_name}', f'{self.cat_infos[cat_name]["imgs"]}', f'{self.cat_infos[cat_name]["anns"]}', f'{round(self.cat_infos[cat_name]["weight"], 2)}']
 
                 # ap
                 precision = precisions[:, :, idx, 0, -1]   # [10, 101]
@@ -237,6 +265,7 @@ class AlchemyCocoDet2dMetric(CocoMetric):
 
                 t.append(f'{round(ap, 3)}')
                 eval_results[f'{cat_name}_ap'] = round(ap, 3)
+                cls_metric.append(round(ap, 3))
 
                 # ar
                 recall = recalls[:, idx, 0, -1]  # [10]
@@ -249,6 +278,7 @@ class AlchemyCocoDet2dMetric(CocoMetric):
 
                 t.append(f'{round(ar, 3)}')
                 eval_results[f'{cat_name}_ar'] = round(ar, 3)
+                cls_metric.append(round(ar, 3))
 
                 # F1
                 if ap != np.nan and ar != np.nan:
@@ -258,11 +288,15 @@ class AlchemyCocoDet2dMetric(CocoMetric):
                 
                 t.append(f'{round(f1, 3)}')
                 eval_results[f'{cat_name}_f1'] = round(f1, 3)
+                cls_metric.append(round(f1, 3))
 
                 # AP@.5 & AP@.75
                 for iou in [0, 5]:
                     precision = precisions[iou, :, idx, 0, -1]
                     precision = precision[precision > -1]
+
+                    r = recalls[iou, idx, 0, -1]
+                    r = max(r, 0)
 
                     if precision.size:
                         ap = np.mean(precision)
@@ -283,6 +317,9 @@ class AlchemyCocoDet2dMetric(CocoMetric):
                         ap = float('nan')
 
                     t.append(f'{round(ap, 3)}')
+                    t.append(f'{round(r, 3)}')
+                    cls_metric.append(round(ap, 3))
+                    cls_metric.append(round(r, 3))
 
                 # indexes of area of small, median and large
                 for area in [1, 2, 3]:
@@ -293,15 +330,13 @@ class AlchemyCocoDet2dMetric(CocoMetric):
                     else:
                         ap = float('nan')
                     t.append(f'{round(ap, 3)}')
+                    cls_metric.append(round(ap, 3))
 
                 results_per_category.append(tuple(t))
+                classwise_metrics.append(cls_metric)
 
             num_columns = len(results_per_category[0])
             results_flatten = list(itertools.chain(*results_per_category))
-
-            headers = [
-                'category', 'images', 'bboxes', 'AP', 'AR', 'F1', 'AP@.5', 'AP@.75', 'AP@small', 'AP@medium', 'AP@large'
-            ]
 
             results_2d = itertools.zip_longest(*[results_flatten[i::num_columns] for i in range(num_columns)])
 
@@ -317,36 +352,54 @@ class AlchemyCocoDet2dMetric(CocoMetric):
             # metric_items = ['mAP', 'mAP@.5', 'mAP@.75', 'mAP@small', 'mAP@medium', 'mAP@large']
             metric_items = list(coco_metric_names.keys())
 
-        heading_line(log='Average Metric', logger=logger)
+        heading_line(log='Average Metric (coco)', logger=logger)
         for metric_item in metric_items:
             val = float(f'{round(coco_eval.stats[coco_metric_names[metric_item]], 3)}')
             eval_results[metric_item] = val
 
             # best
-            if metric_item not in self.best:
-                self.best[metric_item] = (self.current_epoch, val)
+            if metric_item not in self.best_coco:
+                self.best_coco[metric_item] = (self.current_epoch, val)
             else:
-                if val > self.best[metric_item][1]:
-                    self.best[metric_item] = (self.current_epoch, val)
+                if val > self.best_coco[metric_item][1]:
+                    self.best_coco[metric_item] = (self.current_epoch, val)
 
             # print
             current_str = '{: <20}'.format(f'{metric_item}: {val}')
-            best_str = f'(best: {self.best[metric_item][1]} [{self.best[metric_item][0]}])'
+            best_str = f'(best: {self.best_coco[metric_item][1]} [{self.best_coco[metric_item][0]}])'
             logger.info(f'{current_str}{best_str}')
         
         # mF1
         f1 = round(2 * coco_eval.stats[0] * coco_eval.stats[8] / (coco_eval.stats[0] + coco_eval.stats[8] + 1e-4), 3)
-        if 'mF1' not in self.best:
-            self.best['mF1'] = (self.current_epoch, f1)
+        if 'mF1' not in self.best_coco:
+            self.best_coco['mF1'] = (self.current_epoch, f1)
         else:
-            if f1 > self.best['mF1'][1]:
-                self.best['mF1'] = (self.current_epoch, f1)
+            if f1 > self.best_coco['mF1'][1]:
+                self.best_coco['mF1'] = (self.current_epoch, f1)
 
         current_str = '{: <20}'.format(f'mF1: {f1}')
-        best_str = f'(best: {self.best["mF1"][1]} [{self.best["mF1"][0]}])'
+        best_str = f'(best: {self.best_coco["mF1"][1]} [{self.best_coco["mF1"][0]}])'
         logger.info(f'{current_str}{best_str}')
 
         eval_results['mF1'] = f1
+
+        if len(classwise_metrics):
+            heading_line(log='Average Metric (alchemy)', logger=logger)
+            classwise_metrics = np.nan_to_num(np.array(classwise_metrics).reshape(-1, len(headers[4:])))
+            for col_id, metric_item in enumerate(headers[4:]):
+                val = round(float(np.sum(classwise_metrics[:, col_id] * self.cat_weights)), 3)
+
+                # best
+                if metric_item not in self.best_alchemy:
+                    self.best_alchemy[metric_item] = (self.current_epoch, val)
+                else:
+                    if val > self.best_alchemy[metric_item][1]:
+                        self.best_alchemy[metric_item] = (self.current_epoch, val)
+
+                # print
+                current_str = '{: <20}'.format(f'm{metric_item}: {val}')
+                best_str = f'(best: {self.best_alchemy[metric_item][1]} [{self.best_alchemy[metric_item][0]}])'
+                logger.info(f'{current_str}{best_str}')
 
         if tmp_dir is not None:
             tmp_dir.cleanup()
